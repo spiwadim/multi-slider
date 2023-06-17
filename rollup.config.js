23e512200319719a6a46b8c01a37a2590649ee9f
Vadim Spiridenko <vadim.spiridenko@codeworldwide.com>
2023-06-17 21:55:58 +0300

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    postcss()
+  ]
+};
